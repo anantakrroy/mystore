9366fc6756dc6d1875278337f450b571f793949b
Ananta Kr. Roy <kr.innovate@gmail.com>
2021-03-14 12:08:49 +0530

    update authguard on success component router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { ProductsComponent } from './products/products.component';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
 import { CartComponent } from './cart/cart.component';
 import { SuccessComponent } from './success/success.component';
+import { AuthGuardService } from 'src/services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ProductsComponent },
   { path: 'product-list', component: ProductsComponent },
   { path: 'product-detail/:id', component: ProductdetailComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'success', component: SuccessComponent }
+  { path: 'success', component: SuccessComponent,  canActivate:[AuthGuardService] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthGuardService]
 })
 export class AppRoutingModule { }
